bc926ee2ac95c483c6ab04cc6710a9d6557eb665
murilo martins <diasmmurilo@gmail.com>
2025-04-23 17:58:11 -0300

    Semana 08 exercicios, lembrar de fazer o site pessoal com bootstrap e tenta fazer o ex06 de novo

---
+++ Semana07/Exercicio04/MathGame.Maui-Styling/MathGame.Maui/MauiProgram.cs
@@ -0,0 +1,18 @@
+﻿namespace MathGame.Maui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+                fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+				fonts.AddFont("CaveatBrush-Regular.ttf", "CaveatBrush");
+            });
+
+		return builder.Build();
+	}
+}
